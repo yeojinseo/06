7116cf471a8bd733a7d5fb69ab991c6e71a136e7
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-10-11 17:41:43 +0900

    05

--- main.c
+++ main.c
@@ -3,16 +3,44 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+
+int combination(int n, int r)
 {
-	return (a*a);
+	int res;
+	int n_fac, nr_fac, r_fac;
+	
+	n_fac = factorial(n);
+	nr_fac = factorial(n-r);
+	r_fac = factorial(r);
+	
+	res = n_fac / (nr_fac * r_fac);
+	
+	return res;
+}
+
+int factorial(int n)
+{
+	int res = 1;
+	int i;
+	
+	for (i=1; i<=n; i++)
+		res = res * i;
+	
+	return res;
 }
 
 
 int main(int argc, char *argv[]) {
-	int a = 2;
-	a = square(a);
-	printf("a=%i\n", a);
+	int x, y;
+	int final_res;
+	
+	printf("n 값을 입력하시오 : ");
+	scanf("%i", &x);
+	printf("r 값을 입력하시오 : ");
+	scanf("%i", &y);
+	
+	final_res = combination(x, y);
+	printf("%i\n", final_res);
 	
 	return 0;
 }
